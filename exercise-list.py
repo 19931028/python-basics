6ba18f72d47b70349bfaa4e0537921f281659d55
19931028 <lj64349@email.vccs.edu>
2025-07-16 17:30:47 -0400

    Exercise-list

---
+++ exercise-list.py
@@ -0,0 +1,30 @@
+#play with for loops
+hobbies = ["Painting", "Cycling", "Reading", "Coding", "Swimming"]
+print("Here are some hobbies I enjoy:")
+for hobby in hobbies:
+    print("- " + hobby)
+
+
+
+# create § list
+favorite_fruits = ["Cherry", "Mango", "Strawberry", "Peach", "Banana"]
+
+# use a for loop to iterate through the
+print("Here are my favorite fruits:")
+for fruit in favorite_fruits:
+    print("- " + fruit)
+
+
+# create a list
+off_day_list = []
+
+# append items to the list
+off_day_list.append("Do laundry")
+off_day_list.append("Study Python")
+off_day_list.append("Buy groceries")
+off_day_list.append("Cooking")
+
+# use a for loop 
+print("My TODO list:")
+for task in off_day_list:
+    print("- " + task)
